a4b53190ecc7216f0af98a4b41a10f7c37994415
Renan <victorrenan2018@gmail.com>
2025-02-20 17:27:57 -0300

    teste

---
+++ exercicios/src/main/java/fundamentos/teste.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class teste {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(in);
+
+        
+
+
+        scan.close();
+    }
+}
