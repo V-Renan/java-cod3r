6da4754069cbb67db1feaf092b83f810e039b289
Renan <victorrenan2018@gmail.com>
2025-02-21 11:56:59 -0300

    variaveis

---
+++ exercicios/src/main/java/fundamentos/Temperatura.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+/**
+ * @author V-Renan
+ * @date 21/02/2025
+ */
+public class Temperatura {
+    public static void main(String[] args) {
+        // (Fº -32) * 5/9 = ºC
+        final double FATOR = 5.0 / 9
+    }
+}
