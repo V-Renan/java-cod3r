d2b1de5662585667a67b86367684e9f08fc5d858
V-Renan <victorrenan2018@gmail.com>
2025-02-21 21:16:38 -0300

    exercicio de temperatura com variaveis

--- exercicios/src/main/java/fundamentos/Temperatura.java
+++ exercicios/src/main/java/fundamentos/Temperatura.java
@@ -1,12 +1,24 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 /**
  * @author V-Renan
  * @date 21/02/2025
  */
 public class Temperatura {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // (Fº -32) * 5/9 = ºC
-        final double FATOR = 5.0 / 9
+        final double AJUSTE = 32;
+        final double FATOR = 5.0/9.0;
+
+        System.out.print("Digite um valor em fahrenheit: ");
+        double valorFahrenheit = scan.nextDouble();
+
+        double calc = (valorFahrenheit - AJUSTE) * FATOR;
+
+        System.out.printf("%.2f Fahrenheit equivalem a %.2f Celsius.",valorFahrenheit, calc);
+        scan.close();
     }
 }
