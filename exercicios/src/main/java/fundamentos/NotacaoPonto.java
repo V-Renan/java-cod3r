4e546330fb870462859e418934d271893f6dcb35
Renan <victorrenan2018@gmail.com>
2025-02-22 15:43:38 -0300

    notacao ponto

---
+++ exercicios/src/main/java/fundamentos/NotacaoPonto.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+/**
+ * @author V-Renan
+ * @date 22/02/2025
+ */
+public class NotacaoPonto {
+    public static void main(String[] args) {
+
+        String s = "Bom dia X".toUpperCase().replace("X", "Senhora");
+        // s = s.toUpperCase();
+        // s.replace("X", "!");
+        System.out.println(s.concat("!!!"));
+    }
+}
