b71d560f27b53761532e1cc2b256a4b02ba49791
V-Renan <victorrenan2018@gmail.com>
2025-02-23 23:23:50 -0300

    exemplos de importacao

--- exercicios/src/main/java/fundamentos/Import.java
+++ exercicios/src/main/java/fundamentos/Import.java
@@ -1,6 +1,22 @@
-package fundamentos;/**
+package fundamentos;
+
+import java.util.Date;
+
+/**
  * @author V-Renan
  * @date 2/23/2025
  */
 public class Import {
+    public static void main(String[] args) {
+        java.lang.String b = "Boa tarde!";
+        System.out.println(b);
+
+        String s = "Bom dia!";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+        //JButton botao = new JButton();
+    }
 }
