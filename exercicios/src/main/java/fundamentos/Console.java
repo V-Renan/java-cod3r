26d65c9bad61d684684d64a631d802e0a76cb5ca
V-Renan <victorrenan2018@gmail.com>
2025-02-25 20:15:27 -0300

    Salvando alteracoes locais

---
+++ exercicios/src/main/java/fundamentos/Console.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+/**
+ * @author V-Renan
+ * @date 2/24/2025
+ */
+public class Console {
+    public static void main(String[] args) {
+        System.out.print("Bom");
+    }
+}
