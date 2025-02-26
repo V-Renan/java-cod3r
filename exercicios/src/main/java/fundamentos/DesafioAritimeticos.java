c661d56d92a5cf147e7ad228837622b315192169
V-Renan <victorrenan2018@gmail.com>
2025-02-25 23:15:21 -0300

    desafio aritimetico usando Math

---
+++ exercicios/src/main/java/fundamentos/DesafioAritimeticos.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+/**
+ * @author V-Renan
+ * @date 2/25/2025
+ */
+public class DesafioAritimeticos {
+    public static void main(String[] args) {
+
+        int F1 = (6 * (3 + 2));
+        double calculoF1 = Math.pow(F1, 2) / (3 * 2);
+
+        int F2 = ((1 - 5) * (2 - 7));
+        double calculoF2 = Math.pow((double) F2 / 2, 2);
+
+        int resultadoParcial = (int) (calculoF1 - calculoF2);
+
+        int resultadoFinal = (int) (Math.pow(resultadoParcial, 3) / Math.pow(10, 3));
+
+        System.out.printf("Resultado Final: %d", resultadoFinal);
+    }
+}
