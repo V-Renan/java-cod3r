47b642f1b6235271e4671aa1b32a4683536e6834
V-Renan <victorrenan2018@gmail.com>
2025-02-26 23:31:23 -0300

    operadores logicos

--- exercicios/src/main/java/fundamentos/operadores/Logicos.java
+++ exercicios/src/main/java/fundamentos/operadores/Logicos.java
@@ -5,4 +5,8 @@ package fundamentos.operadores;
  * @date 2/26/2025
  */
 public class Logicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = 3 > 7;
+    }
 }
